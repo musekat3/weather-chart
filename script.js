da20ab5cdd36ca045eebebd6553f337d32d36646
Kathy Hyde <musekat3@gmail.com>
2023-07-26 14:42:12 -0700

    Updates to Java and HTML

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // Function to retrieve weather data from an API
 function getWeather() {
     var queryURL= 'https://api.openweathermap.org/data/2.5/forecast?lat={38.941631}&lon={-119.977219}&appid={77e59f20f2615bdf22dfbdc4c4c20a6b}';
-    fetch(queryURL)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${77e59f20f2615bdf22dfbdc4c4c20a6b}&q=${location}`)
+    .then(response => response.json())
+    .then(data => {
+      const { temp_c, condition } = data.current;
+
+      weatherInfo.innerHTML = Temperature: ${temp_c}°C <br> Condition: ${condition.text};
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      weatherInfo.innerHTML = 'An error occurred while fetching weather information.';
+    });
   }; 
   
   function updateUI(data) {
@@ -11,13 +21,13 @@ function getWeather() {
     const iconElement = document.getElementById('icon');
   
     locationElement.innerText = data.location;
-    temperatureElement.innerText = `${data.temperature}°C`;
+    temperatureElement.innerText = ${data.temperature}°C;
     descriptionElement.innerText = data.description;
 
     console.log(data);
 
     window.localStorage.setItem(getWeather);
-  }
-  
-  document.addEventListener('DOMContentLoaded', getWeather);
 
+  }
+
+  document.addEventListener('DOMContentLoaded', getWeather): 
